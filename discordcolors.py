a4b62d2286abddce920275aa41af5eeaf3cf5472
DustinFoes <105510092+DustinFoes@users.noreply.github.com>
2022-09-04 13:29:28 -0500

    Add files via upload

---
+++ discordcolors.py
@@ -0,0 +1,144 @@
+def colorcoder(color):
+    if color == 'Default':
+        return(0)
+    
+    elif color == 'Aqua':
+        return(1752220)
+
+    elif color == 'DarkAqua':
+        return(1146986)
+
+    elif color == 'Green':
+        return(5763719)
+
+    elif color == 'DarkGreen':
+        return(2067276)
+
+    elif color == 'Blue':
+        return(3447003)
+
+    elif color == 'DarkBlue':
+        return(2123412)
+
+    elif color == 'Purple':
+        return(10181046)
+
+    elif color == 'DarkPurple':
+        return(7419530)
+
+    elif color == 'LuminousVividPink':
+        return(15277667)
+
+    elif color == 'DarkVividPink':
+        return(11342935)
+
+    elif color == 'Gold':
+        return(15844367)
+
+    elif color == 'DarkGold':
+        return(12745742)
+
+    elif color == 'Orange':
+        return(15105570)
+
+    elif color == 'DarkOrange':
+        return(11027200)
+
+    elif color == 'Red':
+        return(15548997)
+
+    elif color == 'DarkRed':
+        return(10038562)
+
+    elif color == 'Grey':
+        return(9807270)
+
+    elif color == 'DarkGrey':
+        return(9936031)
+
+    elif color == 'DarkerGrey':
+        return(8359053)
+
+    elif color == 'LightGrey':
+        return(12370112)
+
+    elif color == 'Navy':
+        return(3426654)
+
+    elif color == 'DarkNavy':
+        return(2899536)
+
+    elif color == 'Yellow':
+        return(16776960)
+
+    elif color == 'default':
+        return(0)
+    
+    elif color == 'aqua':
+        return(1752220)
+
+    elif color == 'darkAqua':
+        return(1146986)
+
+    elif color == 'green':
+        return(5763719)
+
+    elif color == 'darkGreen':
+        return(2067276)
+
+    elif color == 'blue':
+        return(3447003)
+
+    elif color == 'darkblue':
+        return(2123412)
+
+    elif color == 'purple':
+        return(10181046)
+
+    elif color == 'darkpurple':
+        return(7419530)
+
+    elif color == 'luminousVividPink':
+        return(15277667)
+
+    elif color == 'darkvividpink':
+        return(11342935)
+
+    elif color == 'gold':
+        return(15844367)
+
+    elif color == 'darkgold':
+        return(12745742)
+
+    elif color == 'orange':
+        return(15105570)
+
+    elif color == 'darkorange':
+        return(11027200)
+
+    elif color == 'red':
+        return(15548997)
+
+    elif color == 'darkred':
+        return(10038562)
+
+    elif color == 'grey':
+        return(9807270)
+
+    elif color == 'darkgrey':
+        return(9936031)
+
+    elif color == 'darkergrey':
+        return(8359053)
+
+    elif color == 'lightgrey':
+        return(12370112)
+
+    elif color == 'navy':
+        return(3426654)
+
+    elif color == 'darknavy':
+        return(2899536)
+
+    elif color == 'yellow':
+        return(16776960)
